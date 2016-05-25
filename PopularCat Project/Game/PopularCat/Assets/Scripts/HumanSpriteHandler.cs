35ce9bb3ee35fe4a50f9f321a0bb0d4a72e74d48
tbdye <thomas@tfenet.com>
2016-05-24 22:58:14 -0700

    Updated Human prefab with HumanSpriteHandler code, but still broken.

--- PopularCat Project/Game/PopularCat/Assets/Scripts/HumanSpriteHandler.cs
+++ PopularCat Project/Game/PopularCat/Assets/Scripts/HumanSpriteHandler.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class HumanSpriteHandler : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    #region Public Fields
+    public Sprite mIdle;
+    public Sprite mAttention;
+    public Sprite mFilming;
+    #endregion
+
+    #region Private Fields
+    SpriteRenderer render;
+    #endregion
+
+    void Start ()
+    {
+        render.sprite = mIdle;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if (GameState.InEncounter)
+        {
+            if (GameState.InFameEncounter)
+            {
+                render.sprite = mFilming;
+            }
+            else
+            {
+                render.sprite = mAttention;
+            }
+        }
+        else
+        {
+            render.sprite = mIdle;
+        }
 	}
 }
